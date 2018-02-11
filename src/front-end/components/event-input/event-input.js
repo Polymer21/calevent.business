565dc1d70bbcd009ec4e5304fa3dc0bff3c592cb
Polymer Trip <polymertrip@gmail.com>
2018-02-11 13:57:54 -0700

    editied scroll & worked on onclick()

--- src/front-end/components/event-input/event-input.js
+++ src/front-end/components/event-input/event-input.js
@@ -4,15 +4,20 @@ import Title from '../events/Title/title'
 import EventOne from '../events/Event-One/event-one'
 import EventTwo from '../events/Event-Two/event-two'
 import EventThree from '../events/Event-Three/event-three'
+import EventFour from '../events/Event-Four/Event-four'
+
 
 class EventInput extends Component {
     render() {
         return (
-            <div className='calendar-container'> 
+            <div className='window'>
+                <div className='window-grid'> 
                     <Title/>
                     <EventOne/>
                     <EventTwo/>
                     <EventThree/>
+                    <EventFour/>
+                </div>
             </div>
         )
     }
--- src/front-end/components/events/Title/title.js
+++ src/front-end/components/events/Title/title.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import './title.css'
+import $ from 'jquery'
+
+$('.title').click(function() {
+    window.location = $(this).find("a").attr("href"); 
+    return false;
+});
 
 class Title extends Component {
     render() {
         return (
-            <div className='title'>
+            <div className='title' ahref='google.com'>
                 <div className='title-instruction'>
-                    <h2 className='title-instruction-words'> Go ahead, select your best date. </h2>
+                    <h2 className='title-instruction-words'>
+                        Go ahead, select your best date. </h2>
                 </div>
                 <div className='date'>
                     <h1 className='current-date'>2.22.18</h1>
